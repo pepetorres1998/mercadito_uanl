660fff8b8a51983431f8b325b09a0bb7522eb8e6
Jose Antonio Torres Garibay <jtorres@syscap.com.mx>
2019-09-12 23:47:05 -0500

    test: Add users controller specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -29,11 +29,21 @@ RSpec.describe UsersController, type: :controller do
   # User. As you add validations to User, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    {
+      name: 'Jose',
+      email: 'jose@torres.com',
+      password: 'asdf',
+      password_confirmation: 'asdf'
+    }
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    {
+      name: '',
+      email: '',
+      password: '',
+      password_confirmation: 'salkdfj'
+    }
   }
 
   # This should return the minimal set of values that should be in the session
