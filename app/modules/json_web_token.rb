4937448d02ce17906481d2f1160e14b084cf0ce2
Jose Antonio Torres Garibay <jtorres@syscap.com.mx>
2019-10-07 00:42:19 -0500

    fix: config set env jwt_key

--- app/modules/json_web_token.rb
+++ app/modules/json_web_token.rb
@@ -4,11 +4,11 @@ class JsonWebToken
   class << self
     def encode(payload, exp = 24.hours.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, Rails.application.credentials.secret_key_base)
+      JWT.encode(payload, ENV['JWT_KEY'])
     end
 
     def decode(token)
-      body = JWT.decode(token, Rails.application.credentials.secret_key_base)[0]
+      body = JWT.decode(token, ENV['JWT_KEY'])[0]
       HashWithIndifferentAccess.new body
     rescue
       nil
